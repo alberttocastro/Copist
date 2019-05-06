eaa73d7f167c4a608f221c158fb122d77f6beb7f
Albertto Castro <albertto.programmer@gmail.com>
2019-05-06 15:17:39 -0300

    Bar

--- resources/views/application/bar.blade.php
+++ resources/views/application/bar.blade.php
@@ -1,1 +1,26 @@
-Action bar here
+<ul id="nav-mobile" class="right hide-on-med-and-down">
+    <li>
+        <a href="/">My Assignments</a> 
+    </li>
+    <li>
+        <a href="/">Assignments</a>
+    </li>
+    <li>
+        <a href="/">Overview</a>
+    </li>
+    <li>
+        <a href="/">Management</a>
+    </li>
+    <li>
+        <a href="/">Permissions</a>
+    </li>
+    <li>
+        <a href="/">Database</a>
+    </li>
+    <li>
+        <a href="/">Help</a>
+    </li>
+    <li>
+        <a href="/">Logout</a>
+    </li>
+</ul>
