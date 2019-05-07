9e4621d2e8fa898eb825cfc1673832a29001d322
Albertto Castro <albertto.programmer@gmail.com>
2019-05-06 21:42:25 -0300

    Created publisher model

---
+++ database/migrations/2019_05_07_004013_create_publishers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublishersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('neighborhood');
+            $table->string('phone');
+            $table->integer('macro_region_id');
+            $table->integer('group_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publishers');
+    }
+}
