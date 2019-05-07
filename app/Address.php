d7751f12177eb040d06e028ab80e119daf88efb2
Albertto Castro <albertto.programmer@gmail.com>
2019-05-07 06:28:09 -0300

    Relacionamentos de address

--- app/Address.php
+++ app/Address.php
@@ -7,4 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     //
+
+    public function card()
+    {
+        return $this->belongsTo('App\Card');
+    }
+
+    public function nationality()
+    {
+        return $this->belongsTo('App\Nationality');
+    }
+
+    public function idiom()
+    {
+        return $this->belongsTo('App\Idiom');
+    }
+
+    public function second_idiom()
+    {
+        return $this->belongsTo('App\Idiom', 'second_language_id');
+    }
+
+    public function address_type(){
+        return $this->belongsTo('App\Address_type');
+    }
+
+    public function publisher(){
+        return $this->belongsTo('App\Publisher');
+    }
+
+    public function macro_region(){
+        return $this->belongsTo('App\Macro_region');
+    }
 }
