392647a199b954133949e0a4ee576d8be2a88fa7
Albertto Castro <albertto.programmer@gmail.com>
2019-05-08 18:37:01 -0300

    Crie método para atualizar as permissões

--- app/Http/Controllers/PermissionsController.php
+++ app/Http/Controllers/PermissionsController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class PermissionsController extends Controller
 {
     //
     public function index(){
         return view('permission.index');
+    }
+
+    public function update(Request $request){
+
+        //Pega da requisição todas as entradas que são estritamente dados
+        foreach($request->except(['_method','_token']) as $key => $value){
+            if(gettype((int)$key) == "integer"){
+                $user = User::find($key);
+                $user->publisher_id = $value;
+
+                $user->save();
+            }
+        }
+
+        return redirect()->action('PermissionsController@index');
     }
 }
