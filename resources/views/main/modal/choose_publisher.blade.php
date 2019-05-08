7a602d78e0dc64d1bd27aafce0d00e7e0a240f15
Albertto Castro <albertto.programmer@gmail.com>
2019-05-08 12:46:50 -0300

    Substitua métodos ruby por blade

--- resources/views/main/modal/choose_publisher.blade.php
+++ resources/views/main/modal/choose_publisher.blade.php
@@ -1,22 +1,33 @@
 <!-- Modal: Escolher irmãos para designar para endereço -->
 <div id="modal-assign-territory" class="modal">
-        <%= form_tag choose_publisher_to_assign_path do %>
-        <%= hidden_field_tag(:card, 0) %>
-            <div class="modal-content">
-                <h4>Assign Territory</h4> <br>
-                <div class="input-field col s12">
-                    <%= select_tag(:publisher_1, options_from_collection_for_select(User.all, :id, :email), {include_blank: "Choose publisher"} ) %>
-                    <label>Publisher 1</label>
-                </div>
-                
-                <div class="input-field col s12">
-                    <%= select_tag(:publisher_2, options_from_collection_for_select(User.all, :id, :email), {include_blank: "Choose publisher"} ) %>
-                    <label>Publisher 2</label>
-                </div>
+    <form action="/" method="post">
+        {{ csrf_field() }}
+        <input type="hidden" name="card">
+        <div class="modal-content">
+            <h4>Assign Territory</h4> <br>
+            <div class="input-field col s12">
+                <select name="publisher_2" id="publisher_1">
+                    <option value="">Choose publisher</option>
+                    @foreach (App\User::all() as $user)
+                        <option value="{{$user->id}}">{{$user->email}}</option>
+                    @endforeach
+                </select>
+                <label>Publisher 1</label>
             </div>
-            <div class="modal-footer">
-                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
-                <%= submit_tag "Send", class:"modal-close waves-effect waves-green btn-flat" %>
+            
+            <div class="input-field col s12">
+                <select name="publisher_2" id="publisher_2">
+                    <option value="">Choose publisher</option>
+                    @foreach (App\User::all() as $user)
+                        <option value="{{$user->id}}">{{$user->email}}</option>
+                    @endforeach
+                </select>
+                <label>Publisher 2</label>
             </div>
         </div>
-        <% end %>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+            <input type="submit" value="Send" class="modal-close waves-effect waves-green btn-flat">
+        </div>
+    </form>
+</div>
