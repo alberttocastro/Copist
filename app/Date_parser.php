b98b7de29da0fbdbb31b03ae0a47cb59241d7e55
Albertto Castro <albertto.programmer@gmail.com>
2019-05-08 10:16:36 -0300

    Created model to deal with date formatting

--- app/Date_parser.php
+++ app/Date_parser.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTime;
 
 class Date_parser extends Model
 {
     //
+    public function from_string_to_datetime(String $string)
+    {
+        return DateTime::createFromFormat("d/m/Y", $string);
+    }
 }
