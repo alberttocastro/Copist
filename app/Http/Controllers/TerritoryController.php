34dca8e4f5b9f856550ada2ce85d6a0720ecb200
Albertto Castro <albertto.programmer@gmail.com>
2019-05-07 21:51:07 -0300

    Controlador de territórios, salvando dados no banco

---
+++ app/Http/Controllers/TerritoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Suggested_address;
+
+class TerritoryController extends Controller
+{
+    //
+
+    public function add_suggested_address(Request $request)
+    {
+        $suggested_address = new Suggested_address;
+        $suggested_address->neighborhood = $request->input('neighborhood');
+        $suggested_address->street = $request->input('street');
+        $suggested_address->name = $request->input('name');
+        $suggested_address->comments = $request->input('comments');
+
+        $suggested_address->save();
+
+        return redirect()->route('home')->with('message', 'Suggested address created successfully');
+    }
+}
