19cc98b4a0d5ffc9e93ef6b40066b1c458984166
Albertto Castro <albertto.programmer@gmail.com>
2019-05-08 14:23:11 -0300

    Crie view para permissão dos usuários

---
+++ resources/views/permission/index.blade.php
@@ -0,0 +1,57 @@
+@extends('application.layout')
+
+@section('header')
+    
+@endsection
+
+@section('main')
+    <div class="container">
+
+        <form action="/" method="put">
+
+            <%# Iteração sobre todos os usuários. Relaciona-os com os seus devidos publicadores %>
+            <div class="row">
+            @foreach(App\User::all() as $user)
+                <div class="col s12 m12">
+                    <div class="card white z-depth-3">
+                        <div class="card-content black-text">
+                            <span class="card-title">{{$user->email}}</span>
+                            <div>
+                                @if($user->publisher_id == null && $user->publisher_id != "" )
+                                    <select name="publisher_{{$user->id}}" id="publisher_{{$user->id}}">
+                                        @foreach(App\Publisher::without_user() as $publisher)
+                                            <option value="{{$publisher->id}}">{{$publisher->name}}</option>
+                                        @endforeach
+                                    </select>
+                                @else
+                                    <select name="publisher_{{$user->id}}" id="publisher_{{$user->id}}">
+                                        @foreach(App\Publisher::without_user() as $publisher)
+                                            @if ($user->publisher_id == $publisher->id)
+                                                <option value="{{$publisher->id}}" selected>{{$publisher->name}}</option>
+                                            @else
+                                                <option value="{{$publisher->id}}">{{$publisher->name}}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                @endif
+                                </br>
+                            </div>
+                            <div class="right-align">
+                                <a href="/{id}" method="delete">REMOVE</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            </div>
+
+            {{-- <%# Botão para enviar as modificações %> --}}
+            <div class="fixed-action-btn">
+                <button type="submit" method="put" class="btn-floating btn-large purple darken-2">
+                    <i class="material-icons Large">send</i>
+                </button>
+            </div>
+
+        </form>
+    </div>
+@endsection
