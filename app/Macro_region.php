feb845d20ca8715649874e612e09a6a9bd472877
Albertto Castro <albertto.programmer@gmail.com>
2019-05-08 13:03:16 -0300

    Crie método para saber quais cartões não estão designados

--- app/Macro_region.php
+++ app/Macro_region.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Macro_region extends Model
 {
     //
+
+    public function cards(){
+        return $this->hasMany('App\Card');
+    }
+
+    /**
+     * Cartões não designados são os que todas as suas designações estão completas,
+     *  isto é, tem data de término
+     */
+    public function unassigned_cards(){
+        
+        $cards = array();
+
+        foreach($this->cards as $id => $card){
+            if(Assignment::where('card_id', $id)->where('completion_date','')->count() == 0){
+                $cards[$id] = $card;
+            }
+        }
+
+        return $cards;
+    }
 }
