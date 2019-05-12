8df7a2b8f9c89649247ca760a3ff1b74fb83c51b
Albertto Castro <albertto.programmer@gmail.com>
2019-05-12 13:14:30 -0300

    Layout changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>{{ config('app.name', 'App') }}</title>
+        <title>{{ config('app.name', 'Copist') }}</title>
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -17,24 +17,24 @@
             <div class="navbar-fixed">
                 <nav class="nav-extended navbar-dark indigo darken-2 z-depth-2">
                     <div class="nav-wrapper">
-                        <a href="{{route('home')}}" class="brand-logo">Coins</a>
+                        <a href="{{route('home')}}" class="brand-logo">{{ config('app.name', 'Copist') }}</a>
                         <a href="#" data-target="slide-out" class="sidenav-trigger">
                             <i class="material-icons">menu</i>
                         </a>
-                        @yield('bar', View::make('application.bar'))
+                        @yield('bar', View::make('layouts.bar'))
                     </div>
                     <div class="nav-content">
                         @yield('header')
                     </div>
                 </nav>
             </div>
-            @yield('sidenav', View::make('application.sidenav'))
+            @yield('sidenav', View::make('layouts.sidenav'))
         </header>
         <main>
             @yield('main')
         </main>
         <footer>
-            @yield('bar', View::make('application.footer'))
+            @yield('bar', View::make('layouts.footer'))
         </footer>
     </body>
     <script src="{{ asset('js/jquery-3.4.1.js') }}" type="text/javascript" defer></script>
