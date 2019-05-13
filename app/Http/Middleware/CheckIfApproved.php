bd3ce54217bfca0329924576b6b96f91435b2dff
Albertto Castro <albertto.programmer@gmail.com>
2019-05-13 18:47:47 -0300

    Registre middlewere de teste de aprovação

---
+++ app/Http/Middleware/CheckIfApproved.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class CheckIfApproved
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Se o usuário tiver ligado a algum publicador, então está aprovado
+        if(Auth::check() && Auth::user()->publisher_id > 0 )
+            return $next($request);
+            
+        return redirect()->action('MainController@not_approved');
+    }
+}
