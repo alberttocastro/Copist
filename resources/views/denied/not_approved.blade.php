7644eefb3948273a60c345434da5bd2c1fa540e4
Albertto Castro <albertto.programmer@gmail.com>
2019-05-13 19:03:42 -0300

    Tela solo de não aprovação

--- resources/views/denied/not_approved.blade.php
+++ resources/views/denied/not_approved.blade.php
@@ -1,1 +1,27 @@
-    You are not allowed yet
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>{{ config('app.name', 'Copist') }}</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/materialize.css') }}" />
+    </head>
+    <body>
+        <header>
+            <div class="navbar-fixed">
+                <nav class="nav-extended navbar-dark indigo darken-2 z-depth-2">
+                    <div class="nav-wrapper">
+                        <a href="{{route('home')}}" class="brand-logo">{{ config('app.name', 'Copist') }}</a>
+                </nav>
+            </div>
+        </header>
+        <main>
+            You are not allowed yet
+        </main>
+    </body>
+    <script src="{{ asset('js/jquery-3.4.1.js') }}" type="text/javascript" defer></script>
+    <script src="{{ asset('js/materialize.js') }}" type="text/javascript" defer></script>
+</html>
