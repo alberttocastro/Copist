b463c40a5af6bfbc96baef65c20e78273dac8fbf
Albertto Castro <albertto.programmer@gmail.com>
2019-05-13 19:07:34 -0300

    Não permita que o usuário acesse as designações se não for administrador

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -5,26 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Card;
 use App\Assignment;
+use Illuminate\Support\Facades\Gate;
 
 class AssignmentController extends Controller
 {
     //
     public function index()
     {
-        $working_cards = array();
-        $cards = Card::all();
-        foreach($cards as $id => $card){
-            if($card->is_at_work()){
-                if(\array_key_exists($card->macro_region_id, $working_cards)){
-                    $macro_region_array = $working_cards[$card->macro_region_id];
-                } else {
-                    $macro_region_array = array();
+        if (Gate::allows('is_user_admin')){
+            $working_cards = array();
+            $cards = Card::all();
+            foreach($cards as $id => $card){
+                if($card->is_at_work()){
+                    if(\array_key_exists($card->macro_region_id, $working_cards)){
+                        $macro_region_array = $working_cards[$card->macro_region_id];
+                    } else {
+                        $macro_region_array = array();
+                    }
+                    $macro_region_array[$card->id] = $card;
+                    $working_cards[$card->macro_region_id] = $macro_region_array;
                 }
-                $macro_region_array[$card->id] = $card;
-                $working_cards[$card->macro_region_id] = $macro_region_array;
             }
+            return view('main.assignments',['working_cards'=>$working_cards]);
         }
-        return view('main.assignments',['working_cards'=>$working_cards]);
+
+        return view('denied.permission_not_granted');
     }
 
     public function assign_to_territory(Request $request)
