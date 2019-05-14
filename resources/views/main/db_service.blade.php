1bad9f6ec2b659d0cfca0d03a60dc3b9d58db888
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 17:59:47 -0300

    Layout básico de banco de dados de serviço

--- resources/views/main/db_service.blade.php
+++ resources/views/main/db_service.blade.php
@@ -19,36 +19,139 @@
 
 @section('main')
 <div id="publishers" class="col s12">
-    <div class="row">
-        <div class="col s6 offset-s3">
-            <h4>Publishers</h4>
-        </div>
+    <div class="container">
         <div class="row">
             <div class="col s12 m10 offset-m1">
-                <div class="row">
-                    @foreach (App\Publisher::all() as $publisher)
-                    <div class="col s12 m6">
-                        <div class="card-panel">
-                            <h5 style="margin-top: 0;">{{ucwords($publisher->name)}}</h5>
-                            <span><b>Email</b> {{$publisher->email}}</span><br>
-                            <span><b>Phone</b> {{$publisher->phone}}</span><br>
-                            <span><b>Neighborhood</b> {{$publisher->neighborhood}}</span><br>
-                            <span><b>Macro Region</b> {{$publisher->macro_region->name}}</span><br>
+                <h4><b>Publishers</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Publisher::all() as $publisher)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{ucwords($publisher->name)}}</h5>
+                                    <span><b>Email</b> {{$publisher->email}}</span><br>
+                                    <span><b>Phone</b> {{$publisher->phone}}</span><br>
+                                    <span><b>Neighborhood</b> {{$publisher->neighborhood}}</span><br>
+                                    <span><b>Macro Region</b> {{$publisher->macro_region->name}}</span><br>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
                         </div>
+                        @endforeach
                     </div>
-                    @endforeach
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Publisher
+                        </span>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <div id="macroregions" class="col s12">
-    4
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10 offset-m1">
+                <h4><b>Macro Regions</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Macro_region::all() as $macro_region)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{ucwords($macro_region->name)}}</h5>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Macro Region
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <div id="cards" class="col s12">
-    5
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10 offset-m1">
+                <h4><b>Cards</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Card::all() as $card)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{$card->name}}</h5>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Card
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <div id="addresstype" class="col s12">
-    6
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10 offset-m1">
+                <h4><b>Address Types</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Address_type::all() as $address_type)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{$address_type->name}}</h5>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Address Type
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
