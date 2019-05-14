34d49e17f50483e61ac0388875b7ab01d080d682
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 13:54:33 -0300

    Conserte ação do controlador para renderizar view correta

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -36,16 +36,16 @@ class MainController extends Controller
     public function db_service()
     {
         if (Gate::allows('is_user_admin'))
-            return view("main.index");
+            return view('main.db_service');
 
-        return view('main.db_service');
+        return view('denied.not_approved');
     }
 
     public function db_public()
     {
         if (Gate::allows('is_user_admin'))
-            return view("main.index");
-            
-        return view('main.db_public');
+            return view('main.db_public');
+        
+        return view('denied.not_approved');
     }
 }
