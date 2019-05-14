28a349fbae685b28d351a3a765a2fa77f2062f09
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 20:04:44 -0300

    Adicione tela de idioma

--- resources/views/main/db_public.blade.php
+++ resources/views/main/db_public.blade.php
@@ -13,7 +13,37 @@
 
 @section('main')
 <div id="idioms" class="col s12">
-    2
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10 offset-m1">
+                <h4><b>Publishers</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Idiom::all() as $idiom)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{$idiom->name}}</h5>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Idiom
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <div id="nationalities" class="col s12">
     3
