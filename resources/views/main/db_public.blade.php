c27a4d1471a44b2c015cb803439f7944aaf0fb8b
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 20:08:18 -0300

    Adicione tela de adicionar nacionalidade

--- resources/views/main/db_public.blade.php
+++ resources/views/main/db_public.blade.php
@@ -46,6 +46,36 @@
     </div>
 </div>
 <div id="nationalities" class="col s12">
-    3
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m10 offset-m1">
+                <h4><b>Publishers</b></h4>
+            </div>
+            <div class="row">
+                <div class="col s12 m10 offset-m1">
+                    <div class="row">
+                        @foreach (App\Nationlity::all() as $nationlity)
+                        <div class="col s12 m6">
+                            <div class="card">
+                                <div class="card-content">
+                                    <h5 style="margin-top: 0;">{{$nationlity->name}}</h5>
+                                </div>
+                                <div class="card-action">
+                                    <a class="blue-text text-darken-4" href="#">Edit</a>
+                                    <a class="red-text text-darken-4" href="#">Delete</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                    <a href="/" class="btn blue accent-4 waves-effect waves-light col s12 center-align">
+                        <span class="valign-wrapper " style="max-width:fit-content; margin: auto;">
+                            <i class="material-icons">add_circle_outline</i> Add Nationality
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
