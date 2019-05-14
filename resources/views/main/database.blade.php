0860816d9b9d53d8f2734bf177d8214ef89b2008
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 13:20:51 -0300

    Initial database layout

--- resources/views/main/database.blade.php
+++ resources/views/main/database.blade.php
@@ -1,9 +1,45 @@
 @extends('layouts.app')
 
 @section('header')
-    
+<ul class="tabs tabs-transparent">
+    <li class="tab">
+        <a href="#publishers" class="active">Publishers</a>
+    </li>
+    <li class="tab">
+        <a href="#idioms">Idioms</a>
+    </li>
+    <li class="tab">
+        <a href="#nationalities">Nationalities</a>
+    </li>
+    <li class="tab">
+        <a href="#macroregions">Macro Region</a>
+    </li>
+    <li class="tab">
+        <a href="#cards">Cards</a>
+    </li>
+    <li class="tab">
+        <a href="#addresstype">Address Types</a>
+    </li>
+</ul>
 @endsection
 
 @section('main')
-    
+<div id="publishers" class="col s12">
+    1
+</div>
+<div id="idioms" class="col s12">
+    2
+</div>
+<div id="nationalities" class="col s12">
+    3
+</div>
+<div id="macroregions" class="col s12">
+    4
+</div>
+<div id="cards" class="col s12">
+    5
+</div>
+<div id="addresstype" class="col s12">
+    6
+</div>
 @endsection
