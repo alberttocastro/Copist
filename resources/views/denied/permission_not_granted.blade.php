9350a834cd58de63f1ead7a2978545c044cd5463
Albertto Castro <albertto.programmer@gmail.com>
2019-05-14 11:44:47 -0300

    Redesenhe tela de não permissão de acessar uma função

--- resources/views/denied/permission_not_granted.blade.php
+++ resources/views/denied/permission_not_granted.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('main')
-    You are not permitted to access this function
+    <div class="row">
+        <div class="col s12">
+            <h2 class="center-align" style="width: 100%">
+                Sorry :(
+            </h2>
+        </div>
+        <div class="col s12">
+            <h5 class="center-align" style="width: 100%">
+                You are not permitted to access this function
+            </h5>
+        </div>
+    </div>
 @endsection
