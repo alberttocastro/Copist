8656f798e63a959c4de96d44fe0c157c9a9d2626
Albertto Castro <albertto.programmer@gmail.com>
2019-05-20 19:58:33 -0300

    Use dados vindos do controlador para gerar os gráficos

--- resources/views/main/overview.blade.php
+++ resources/views/main/overview.blade.php
@@ -3,7 +3,7 @@
 @section('header')
 <ul class="tabs tabs-transparent">
     <li class="tab">
-        <a href="#dashboard"  class="active">Insights</a>
+        <a href="#dashboard" class="active">Insights</a>
     </li>
     <li class="tab">
         <a href="#all">Addresses</a>
@@ -16,6 +16,8 @@
 {{-- <%#Script dos gráficos%> --}}
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
 
+
+
 <!-- ABA 1 -->
 <div id="all" class="col s12">
     <div class="container">
@@ -25,7 +27,7 @@
         <h4>Macro Region and Addresses</h4>
         @foreach (App\Macro_region::all() as $macro_region)
         <blockquote>
-            <h5>{{$macro_region->name}}</h5>
+            <h5>{{$macro_region->name}}</h5> <span>({{$macro_region->addresses_quantity()}})</span>
         </blockquote>
 
         <ul class="collapsible popout">
@@ -43,7 +45,8 @@
                                     <p>{{$address->street}}</p>
                                 </div>
                                 <div class="s4">
-                                    <a href="{{route('edit_territory',['id'=>$address->id])}}" class="waves-effect waves-light btn right">Edit</a>
+                                    <a href="{{route('edit_territory',['id'=>$address->id])}}"
+                                        class="waves-effect waves-light btn right">Edit</a>
                                 </div>
                             </div>
                         </div>
@@ -74,7 +77,8 @@
                                     <p>{{$address->street}}</p>
                                 </div>
                                 <div class="s4">
-                                    <a href="{{route('edit_territory',['id'=>$address->id])}}" class="waves-effect waves-light btn right">Edit</a>
+                                    <a href="{{route('edit_territory',['id'=>$address->id])}}"
+                                        class="waves-effect waves-light btn right">Edit</a>
                                 </div>
                             </div>
                         </div>
@@ -85,7 +89,8 @@
             @endforeach
             {{-- Addresses sem card --}}
             <li>
-                <div class="collapsible-header"><i class="material-icons red-text">place</i><span class="red-text"><b>Addresses without cards</b></span></div>
+                <div class="collapsible-header"><i class="material-icons red-text">place</i><span
+                        class="red-text"><b>Addresses without cards</b></span></div>
                 <div class="collapsible-body">
                     @foreach (App\Address::where('card_id', 0)->get() as $address)
                     <div class="card blue-grey darken-1">
@@ -97,7 +102,8 @@
                                     <p>{{$address->street}}</p>
                                 </div>
                                 <div class="s4">
-                                    <a href="{{route('edit_territory',['id'=>$address->id])}}" class="waves-effect waves-light btn right">Edit</a>
+                                    <a href="{{route('edit_territory',['id'=>$address->id])}}"
+                                        class="waves-effect waves-light btn right">Edit</a>
                                 </div>
                             </div>
                         </div>
@@ -127,36 +133,29 @@
         <h3><b>Charts</b></h3>
         <div class="divider"></div>
         <div class="col"></div>
+        <div class="row">
+            <div class="col l10 m6">
+                <h4>Addresses</h4>
+            </div>
+            <div class="col l2 m3 offset-m3">
+                <span class="right">
+                    <h4>{{App\Address::where('is_valid', true)->count()}}</h4>
+                </span>
+            </div>
+        </div>
+        <div class="divider"></div>
         <h4 class="grey-text text-darken2">By Country</h4>
 
-        <canvas id="foreigners-by-country" width="400" height="400"></canvas>
+        <canvas id="foreigners-by-country" width="20rem" height="10rem"></canvas>
         <script>
             var ctx = document.getElementById("foreigners-by-country");
                 var myChart = new Chart(ctx, {
                     type: 'pie',
                     data: {
-                        labels: ["Ghana", "Nigeria", "South Africa", "USA", "England", "Japan", "Others"],
+                        labels: <?php echo $nationality_names ?>,
                         datasets: [{
                             label: '# foreigners',
-                            data: [20, 18, 1, 30, 7, 2, 30],
-                            backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)',
-                            'rgba(50, 50, 50, 0.2)'
-                            ],
-                            borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)',
-                            'rgba(50, 50, 50, 1)'
-                            ],
+                            data:<?php echo $nationality_quantities ?>,
                             borderWidth: 1
                         }]
                     }
@@ -165,43 +164,6 @@
         <br>
 
         <div class="divider"></div>
-        {{-- <%# Título %> --}}
-        <div class="col"></div>
-        <h4 class="grey-text text-darken2">Regions and attendance</h4>
-
-        <canvas id="foreigners-by-region" width="400" height="400"></canvas>
-        <script>
-            var ctx = document.getElementById("foreigners-by-region");
-                var myChart = new Chart(ctx, {
-                    type: 'radar',
-                    data: {
-                        labels: ["Centro", "Barra", "Litoral Sul", "Litoral Centro", "Litoral Norte", "Paralela Centro", "Paralela Norte", "Cidade baixa", "Suburbio"],
-                        datasets: [
-                        {
-                            label: "Quantidade",
-                            fillColor: "rgba(220,220,220,0.2)",
-                            strokeColor: "rgba(220,220,220,1)",
-                            pointColor: "rgba(220,220,220,1)",
-                            pointStrokeColor: "#fff",
-                            pointHighlightFill: "#fff",
-                            pointHighlightStroke: "rgba(220,220,220,1)",
-                            data: [30, 40, 23, 10, 4, 5, 6, 5, 5]
-                        },
-                        {
-                            label: "Frequência de Trabalho",
-                            fillColor: "rgba(151,187,205,0.2)",
-                            strokeColor: "rgba(151,187,205,1)",
-                            pointColor: "rgba(151,187,205,1)",
-                            pointStrokeColor: "#fff",
-                            pointHighlightFill: "#fff",
-                            pointHighlightStroke: "rgba(151,187,205,1)",
-                            data: [40, 40, 30, 20, 15, 5, 5, 15, 5]
-                        }
-                        ]
-                    }
-                });
-        </script>
-
 
     </div>
 </div>
