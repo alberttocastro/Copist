d841e745baccf5daaf84d32053ac5713068b83dc
Albertto Castro <albertto.programmer@gmail.com>
2019-05-20 19:58:04 -0300

    Envie dados de graficos para a tela de overview

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use App\Address;
+use App\Nationality;
 
 class MainController extends Controller
 {
@@ -14,7 +16,7 @@ class MainController extends Controller
     {
         if (Gate::allows('is_user_approved'))
             return view("main.index");
-        
+
         return view('denied.not_approved');
     }
 
@@ -25,7 +27,21 @@ class MainController extends Controller
 
     public function overview()
     {
-        return view('main.overview');
+        $nationality_names = array();
+        $nationality_quantities = array();
+        $nationality_colors = array();
+
+        foreach(Nationality::all() as $nationality)
+        {
+            array_push($nationality_names, $nationality->name);
+            array_push($nationality_quantities, $nationality->addresses->count());
+            array_push($nationality_colors, "rgba(".rand(0,256).",".rand(0,256).",".rand(0,256).","."0.2");
+        }
+
+        return view('main.overview')
+                    ->with('nationality_quantities', json_encode($nationality_quantities))
+                    ->with('nationality_names', json_encode($nationality_names))
+                    ->with('nationality_colors', json_encode($nationality_colors));
     }
 
     public function not_approved()
