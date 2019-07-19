c095284def32ea14e0167762d9309ba477735638
Albertto Castro <albertto.programmer@gmail.com>
2019-07-19 15:48:35 -0300

    Consertando espaçamento de input de formulário

--- resources/views/main/modal/choose_publisher.blade.php
+++ resources/views/main/modal/choose_publisher.blade.php
@@ -5,25 +5,28 @@
         <input type="hidden" id="card_id" name="card_id">
         <div class="modal-content">
             <h4>Assign Territory</h4> <br>
-            <div class="input-field col s12">
-                <select name="publisher_1" id="publisher_1">
-                    <option value="">Choose publisher</option>
-                    {{-- Usuários aprovados são os que estão ligados a um publicador da congregação --}}
-                    @foreach (App\User::approved_users() as $user)
+            <div class="row">
+                <div class="input-field col s12">
+                    <select name="publisher_1" id="publisher_1">
+                        <option value="">Choose publisher</option>
+                        {{-- Usuários aprovados são os que estão ligados a um publicador da congregação --}}
+                        @foreach (App\User::approved_users() as $user)
                         <option value="{{$user->id}}">{{$user->publisher->name}}</option>
-                    @endforeach
-                </select>
-                <label>Publisher 1</label>
+                        @endforeach
+                    </select>
+                    <label>Publisher 1</label>
+                </div>
             </div>
-
-            <div class="input-field col s12">
-                <select name="publisher_2" id="publisher_2">
-                    <option value="0">Choose publisher</option>
-                    @foreach (App\User::approved_users() as $user)
+            <div class="row">
+                <div class="input-field col s12">
+                    <select name="publisher_2" id="publisher_2">
+                        <option value="0">Choose publisher</option>
+                        @foreach (App\User::approved_users() as $user)
                         <option value="{{$user->id}}">{{$user->publisher->name}}</option>
-                    @endforeach
-                </select>
-                <label>Publisher 2</label>
+                        @endforeach
+                    </select>
+                    <label>Publisher 2</label>
+                </div>
             </div>
         </div>
         <div class="modal-footer">
