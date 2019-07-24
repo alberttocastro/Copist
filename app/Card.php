d4e9be1f9ca1605e37cd7fbc78c2aaa142d8337c
Albertto Castro <albertto.programmer@gmail.com>
2019-07-24 18:51:49 -0300

    Crie função para retornar todos os designados para o cartão

--- app/Card.php
+++ app/Card.php
@@ -19,7 +19,7 @@ class Card extends Model
 
     public function is_at_work()
     {
-        return Assignment::where('card_id',$this->id)->where('completion_date', null)->count() > 0;
+        return Assignment::where('card_id',$this->id)->whereNull('completion_date')->count() > 0;
     }
 
     public function get_people_assigned_to_the_card()
@@ -37,5 +37,24 @@ class Card extends Model
             }
         }
         return $people;
+    }
+
+    public function get_assignments_to_card()
+    {
+        $assignments = array();
+        foreach(Assignment::where('card_id',$this->id)->whereNull('completion_date')->get() as $assignment){
+            if($assignment->user != null )
+            {
+                $assignment->user = $assignment->user;
+            } else {
+                // Caso o usuário não esteja mais no banco de dados, previne que dê erro
+                $user = new \App\User;
+                $user->name = "Unknown";
+                $assignment->user = $user;
+            }
+            $assignments[] = $assignment;
+        }
+
+        return $assignments;
     }
 }
