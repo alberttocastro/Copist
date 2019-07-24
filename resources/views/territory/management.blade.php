b57917c3322ae5d985f4e934ff2475ae6270e5c2
Albertto Castro <albertto.programmer@gmail.com>
2019-07-24 13:09:17 -0300

    Use componente vue

--- resources/views/territory/management.blade.php
+++ resources/views/territory/management.blade.php
@@ -27,78 +27,9 @@
 <!-- Aba para escolher o mapa dos endereços -->
 <div id="manage">
     <div class="container">
-
-        @if (App\Address::all()->where('card_id', 0)->count() > 0)
-        <blockquote>
-            <h4>Address without a map</h4>
-        </blockquote>
-        @endif
-
-        <!-- Cartão de novo território -->
-        @forelse(App\Address::where('card_id', 0)->get() as $address)
-        <div class="row">
-            <div class="col s12 m6">
-                <div class="card grey lighten-5 z-depth-2">
-                    <div class="card-content indigo-text text-darken-4">
-                        <p>
-                            <b>Street:</b> <span>{{ $address->street }}</span>
-                        </p>
-                        <p>
-                            <b>Neighborhood:</b> <span>{{ $address->neighborhood }}</span>
-                        </p>
-                        @if($address->references != "")
-                        <p>
-                            <b>Reference:</b> <span>{{ $address->reference }}</span>
-                        </p>
-                        @endif
-                        @if($address->comments != "")
-                        <p>
-                            <b>Comments:</b> <span> {{$address->comments}} </span>
-                        </p>
-                        @endif
-                    </div>
-                    <div class="card-action">
-                        <a id="{{$address->id}}" href="#choose-map"
-                            class="modal-trigger add-card-to-address-button">Choose card</a>
-                    </div>
-                </div>
-            </div>
+        <div id="vue4">
+            <choose-address-card></choose-address-card>
         </div>
-        @empty
-
-        <div class="valign-wrapper">
-            <div style="width: 100%">
-                <h2 class="center-align">Nice!</h2>
-                <h5 class="center-align">All addresses have their own map.</h5>
-                <p class="center-align">That's beautiful!</p>
-            </div>
-        </div>
-
-        @endforelse
-
-        <form action="{{route('set_map_to_address')}}" method="post">
-            @method('put')
-            @csrf
-            <!-- Modal - Escolher mapa -->
-            <div id="choose-map" class="modal">
-                <!-- Conteúdo -->
-                <div class="modal-content">
-                    <input type="hidden" id="address_id" name="address_id">
-                    <h4>Choose map</h4> <br>
-                    <div class="input-field col s12">
-                        <select name="card_id" id="card_id">
-                            @foreach (App\Card::all() as $card)
-                            <option value="{{$card->id}}">{{$card->name}} - {{$card->number}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <!-- Footer -->
-                <div class="modal-footer">
-                    <input type="submit" value="Confirm" class="modal-close waves-effect waves-green btn-flat">
-                </div>
-            </div>
-        </form>
     </div>
 </div>
 @endsection
