4c1df171d1c6f17cd74d09e5c9f4b9e477346d2b
Albertto Castro <albertto.programmer@gmail.com>
2019-08-28 16:38:58 -0300

    logout

--- resources/js/stores/index.js
+++ resources/js/stores/index.js
@@ -10,6 +10,39 @@ export const store = new Vuex.Store({
         access_token: window.localStorage.getItem('access_token') || '',
         refresh_token: window.localStorage.getItem('refresh_token') || '',
         expire_at: window.localStorage.getItem('expire_at') || '',
+        user: {
+            name: '',
+            email: '',
+        }
+    },
+    mutations: {
+        save_credentials(state, payload) {
+            state.access_token = payload.access_token;
+            state.refresh_token = payload.refresh_token;
+            state.expire_at = new Date(Date.now() + payload.expires_in);
+
+            window.localStorage.setItem(
+                "refresh_token",
+                payload.refresh_token
+            );
+            window.localStorage.setItem(
+                "access_token",
+                payload.access_token
+            );
+            window.localStorage.setItem(
+                "token_expire_time",
+                new Date(Date.now() + payload.expires_in)
+            );
+        },
+        delete_credentials(state, payload) {
+            state.access_token = '';
+            state.refresh_token = '';
+            state.expire_at = '';
+
+            window.localStorage.removeItem('access_token');
+            window.localStorage.removeItem('refresh_token');
+            window.localStorage.removeItem('expire_at');
+        }
     },
     actions: {
         api_authenticate(state, payload) {
@@ -27,34 +60,22 @@ export const store = new Vuex.Store({
             return instance
                 .post(payload.url, qs.stringify(data), {
                     headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
+                        "Content-Type": "application/x-www-form-urlencoded",
                     }
                 })
                 .then(response => {
-                    state.access_token = response.data.access_token;
-                    state.refresh_token = response.data.refresh_token;
-                    state.expire_at = new Date(Date.now() + response.data.expires_in);
-
-                    window.localStorage.setItem(
-                        "refresh_token",
-                        response.data.refresh_token
-                    );
-                    window.localStorage.setItem(
-                        "access_token",
-                        response.data.access_token
-                    );
-                    window.localStorage.setItem(
-                        "token_expire_time",
-                        new Date(Date.now() + response.data.expires_in)
-                    );
+                    this.commit('save_credentials', response.data);
 
                     return response;
                 })
         },
+        logout(state) {
+            this.commit('delete_credentials');
+        }
     },
     getters: {
         isLoggedIn(state) {
-            return state.access_token == '' || (state.access_token.expire_at != '' && Date.now() > state.expire_at)
+            return state.access_token != '' || (state.expire_at != '' && Date.now() > state.expire_at)
         }
     }
 })
