6918ea8afee57f8d2614b40d55011cc5675c6dcc
Albertto Castro <albertto.programmer@gmail.com>
2019-09-04 18:34:32 -0300

    Logs

--- config/logging.php
+++ config/logging.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'slack'),
+    'default' => env('LOG_CHANNEL', 'single'),
 
     /*
     |--------------------------------------------------------------------------
