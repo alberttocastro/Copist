faa08e1b748a25b94c21ece088addc4341b00740
Albertto Castro <albertto.programmer@gmail.com>
2019-09-17 19:50:47 -0300

    migration para valor default em enderessos

---
+++ database/migrations/2019_09_17_224812_default_values_to_address.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DefaultValuesToAddress extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->string('neighborhood')->nullable(true)->default(null)->change();
+            $table->string('street')->nullable(true)->default(null)->change();
+            $table->integer('nationality_id')->nullable(true)->default(null)->change();
+            $table->integer('idiom_id')->nullable(true)->default(null)->change();
+            $table->integer('address_type_id')->nullable(true)->default(null)->change();
+            $table->integer('publisher_id')->nullable(true)->default(null)->change();
+            $table->integer('second_language_id')->nullable(true)->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('address', function (Blueprint $table) {
+            //
+        });
+    }
+}
